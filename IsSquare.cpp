8f1a1b7143762e350bd348e267365aaa4cccf0f0
Charlene Jiang <charlene@tsynka.com>
2012-10-24 19:07:13 +0800

    Implement isSquare!

--- IsSquare.cpp
+++ IsSquare.cpp
@@ -15,17 +15,35 @@ struct Point {
   Point (int _x, int _y) : x(_x), y(_y) {}
 };
 
-bool isSquare(const Point &lt, const Point &rb)
+int square(int n)
 {
-  return (abs(rb.x - lt.x) == abs(lt.y - rb. y)) && rb.x != lt.x;
+  return n * n;
+}
+
+int dis(const Point &p1, const Point &p2)
+{
+  return square(p1.x - p2.x) + square(p1.y - p2.y);
+}
+
+int sum(const Point &p1, const Point &p2, const Point &p3)
+{
+  cout << dis(p1, p2) + dis(p1, p3) + dis(p2, p3) << endl;
+  return dis(p1, p2) + dis(p1, p3) + dis(p2, p3);
+}
+
+bool isSquare(const Point &p1, const Point &p2, const Point &p3, const Point &p4)
+{
+  return sum(p1, p2, p3) == sum(p1, p2, p4) &&
+  sum(p1, p2, p3) == sum(p1, p3, p4) 
+  && sum(p1, p2, p3) == sum(p2, p3, p4);
 }
 
 int main()
 {
-  Point lt, rb;
+  Point p1, p2, p3, p4;
   
-  while (cin >> lt.x >> lt.y >> rb.x >> rb.y) {    
-    if (isSquare(lt, rb)) {
+  while (cin >> p1.x >> p1.y >> p2.x >> p2.y >> p3.x >> p3.y >> p4.x >> p4.y) {    
+    if (isSquare(p1, p2, p3, p4)) {
       cout << "YES" << endl;
     } else {
       cout << "NO" << endl;
