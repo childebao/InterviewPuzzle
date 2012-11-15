b43dbc5141171c4cb6005d4ad210629b7db64f59
Charlene Jiang <charlene@tsynka.com>
2012-11-16 01:28:59 +0800

    Add comments...

--- Timus/1917.cpp
+++ Timus/1917.cpp
@@ -1,5 +1,28 @@
 /*
  Timus 1917: http://acm.timus.ru/problem.aspx?space=1&num=1917
+ 
+ From the problem description, the Annihilation Spell destroys all the coins whose resistance limit is no greater than the power of the spell. 
+ 
+ We could draw some conclusions as:
+ 1. The power of spell should increase each time.
+ 2. The power of each spell should be the ones of the coins' resistance limits. Because there is no need to be larger than the maximum limits of the coins we'd like to destroy at the time.
+ 3. The wizard could no longer give a spell if we meet the kind of coins with N coins of L each, and N * L > P (P is the maximum power the he can survive)
+ 
+ Method 1: BFS().
+ This is a Graph theory algorithm.
+ Now sort the coins limits, and count the coins of M kinds, nodes[M], adding another kind with 0 coin of 0 limits at the beginning.
+ We can construct a directed Graph, if there is an edge from node i to node j, so that the number of coins greater than limit[i] and less and equal than limits[j] is count, count * limit[j] <= P.
+ We could use Breadth First Search to find the maximum limit that could be reached from 0 to M.
+ 
+ Method 2: deal().
+ This is a greedy algorithm.
+ As from the 3 conclusion, we could prove that each time choose as many coins as we can to destoy, until we meet the kind of coins whose total limits is greater than P.
+ We could prove the using this greedy algorithm, we could get the minimum spells and the maximum coins we could destroy.
+ 
+ For each spell, we choose some kinds of coins to destroy, until we meet the kind of coins with N coins and L limits, where N * L > P, which could get the maximum coins we could desotry. This is obvious because if we choose the kind with greater limits coins, as we could not destroy them last time, so when destroying the greater limits those left coins would be destroyed together, and the power they give back would be greater, too, that's impossible. So the greedy algorithm could get the maxmum coins we could destroy.
+ For each spell, we choose as many kinds of coins as we can to destoy, which could get the minimum spell we destroy those coins. If we could destroy those M coins with N spells, and now we don't choose as many as we can, for example, we could choose the first two kinds of coins, but we just choose the first one, and then we left M - count1 coins, which is greater than M - count1 - count2 if we choose the first two. If we could destroy those M - count1 in less than N - 1 spells, it is obvious that we could destroy those M - count1 - count2 coins in less than N - 1. But that's impossible. 
+ So the greedy algorithm is correct.
+ 
  */
 
 #include <iostream>
