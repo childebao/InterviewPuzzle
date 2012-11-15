d3ff5f22e73fe5dbd6faa8b097dc0f6f0b0329fa
Charlene Jiang <charlene@tsynka.com>
2012-11-15 16:29:21 +0800

    Shorten the code..

--- Timus/1917.cpp
+++ Timus/1917.cpp
@@ -5,34 +5,26 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <queue>
-#include <utility>
-#include <map>
 
 using namespace std;
-int N, P, i, j, tmp, h[1000100];
+int N, P, i, h[1000100];
 vector<pair<int, int> > nodes;
 
 void deal(const int & P)
 {
-  int maxCoins = 0, minSpells = 0;
+  int maxCoins = 0, minSpells = 0, tmp = 0;
 
-  for (i = 0; i < nodes.size();) {
-    tmp = 0;
-    
-    for (j = i + 1; j < nodes.size(); j ++) {
-      tmp += nodes[j].second;
-      if (tmp * nodes[j].first > P) {
-        tmp -= nodes[j].second;
-        break;
-      }
-    }
-    
-    if (j == i + 1) break;
-    else {
-      i = j - 1;
+  for (i = 1; i < nodes.size(); i ++) {
+    tmp += nodes[i].second;
+    if (tmp * nodes[i].first > P) {
+      tmp -= nodes[i --].second;
+      if (tmp == 0) break; else maxCoins += tmp;
+      
       minSpells ++;
+      tmp = 0;
+    } else if (i == nodes.size() - 1) {
       maxCoins += tmp;
+      minSpells ++;
     }
   }
 
@@ -42,10 +34,10 @@ void deal(const int & P)
 int main()
 {
   cin >> N >> P;
-  while (N -- && cin >> tmp) h[tmp] ++;
+  while (N -- && cin >> i) h[i] ++;
   
   nodes.push_back(make_pair(0, 0));
-  for (int i = 0; i < 1000000; i ++) {
+  for (i = 0; i < 1000000; i ++) {
     if (h[i]) nodes.push_back(make_pair(i, h[i]));
   }
   
