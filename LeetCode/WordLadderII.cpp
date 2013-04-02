25c3e9d920e210bca7669f4bcbddc6d8d72feac9
Charlene Jiang <charlene@tsynka.com>
2013-04-02 11:22:19 +0800

    Not correct...

--- LeetCode/WordLadderII.cpp
+++ LeetCode/WordLadderII.cpp
@@ -3,17 +3,18 @@ public:
 
     map<string, vector<string> > next;
     vector<vector<string> > ans;
+    int minimalLevel;
     
     void bfs(string start, string end, unordered_set<string> &dict)
     {
-        map<string, bool> visited;
-        visited[start] = true;
+        map<string, int> visited;
+        visited[start] = 1;
         next.clear();
         
         queue<pair<string, int> >q;
         q.push(make_pair(start, 1));
         
-        int minimalLevel = dict.size();
+        minimalLevel = dict.size();
         
         while (!q.empty()) {
             
@@ -21,21 +22,27 @@ public:
             q.pop();
             string tmp = node.first;
             
-            if (node.first == end) {
-                minimalLevel = node.second;
-            }
-            
             if (node.second + 1 > minimalLevel) return;
+            
+            map<string, bool> tmpmap;
             
             for (int i = 0; i < start.length(); i ++) {
                 for (int j = 'a'; j <= 'z'; j ++) {
                     if (j != node.first[i]) {
                         tmp[i] = j;
                         
-                        if (dict.find(tmp) != dict.end() && !visited[tmp]) {
-                            visited[tmp] = true;
-                            q.push(make_pair(tmp, node.second + 1));
-                            next[node.first].push_back(tmp);
+                        if (dict.find(tmp) != dict.end() && !tmpmap[tmp] && (!visited[tmp] || visited[tmp] == node.second + 1)) {
+                            if (tmp == end) {
+                                if (minimalLevel >= node.second + 1) {
+                                    minimalLevel = node.second + 1;   
+                                    next[node.first].push_back(tmp);
+                                }
+                            } else {
+                                tmpmap[tmp] = true;
+                                visited[tmp] = node.second + 1;
+                                q.push(make_pair(tmp, node.second + 1));
+                                next[node.first].push_back(tmp);
+                            }
                         }
                         
                         tmp[i] = node.first[i];
@@ -46,17 +53,19 @@ public:
         
     }
     
-    void output(string start, string end, vector<string> & strs)
-    {
+    void output(string start, string end, vector<string> & strs, int level)
+    {   
         if (start == end) {
             strs.push_back(start);
-            ans.push_back(strs);
+            if (level == minimalLevel) {
+                ans.push_back(strs);
+            }
             return;
         }
         
         strs.push_back(start);
         for (int i = 0; i < next[start].size(); i ++) {
-            output(next[start][i], end, strs);
+            output(next[start][i], end, strs, level + 1);
             strs.pop_back();
         }
     }
@@ -71,7 +80,7 @@ public:
         vector<string> strs;
         ans.clear();
         
-        output(start, end, strs);
+        output(start, end, strs, 1);
         
         return ans;
     }
