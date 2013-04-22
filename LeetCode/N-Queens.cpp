1736ebc301ef890cfee77c33a0c07eff3a365a0d
Charlene Jiang <charlene@tsynka.com>
2013-04-22 18:29:52 +0800

    Add non-bitmapSolve function...

--- LeetCode/N-Queens.cpp
+++ LeetCode/N-Queens.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
+    vector<vector<string> > ans;
     
-    void solve(vector<vector<string> > ans, int n, int curr, long long check, vector<int> & columns) {
+    void bitmapSolve(int n, int curr, int checkCols, int checkDiag1, int checkDiag2, vector<int> & columns) {
         if (n == curr) {
             vector<string> ret(n, string(n, '.'));
             
@@ -14,27 +15,59 @@ public:
         }
         
         for (int i = 0; i < n; i ++) {
+            if (checkCols & (1 << i)) continue;
+            if (checkDiag1 & (1 << (i + curr))) continue;
+            if (checkDiag2 & (1 << (n + i - curr))) continue;
             
-            if (check & (1 << i)) continue;
-            if (check & (1 << (n + i + curr))) continue;
-            if (check & (1 << (4 * n + i - curr))) continue;
-            
-            int tmpCheck = check | (1 << i);
-            tmpCheck |= (1 << (n + i + curr));
-            tmpCheck |= (1 << (4 * n + i - curr));
+            long long tmpCheck = checkDiag2;
+            tmpCheck |= (1 << (n  + i - curr));
             
             columns[curr] = i;
-            solve(ans, n, curr + 1, tmpCheck, columns);
+            solve(n, curr + 1, checkCols | (1 << i), checkDiag1 | (1 << (i + curr)), checkDiag2 | (1 << (n  + i - curr)), columns);
         }
     }
+    
+    void solve(int n, int curr, vector<bool> & check, vector<int> & columns) {
+        if (n == curr) {
+            vector<string> ret(n, string(n, '.'));
+            
+            for (int i = 0; i < n; i ++) {
+                ret[i][columns[i]] = 'Q';
+            }
+            
+            ans.push_back(ret);
+            return;
+        }
+        
+        for (int i = 0; i < n; i ++) {
+            if (check[i]) continue;
+            if (check[n + i + curr]) continue;
+            if (check[4 * n + i - curr]) continue;
+            
+            check[i] = true;
+            check[n + i + curr] = true;
+            check[4 * n + i - curr] = true;
+            columns[curr] = i;
+            
+            solve(n, curr + 1, check, columns);
+            
+            // backtracking
+            check[i] = false;
+            check[n + i + curr] = false;
+            check[4 * n + i - curr] = false;
+        }
+    }
+    
     
     vector<vector<string> > solveNQueens(int n) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        vector<vector<string> > ans;
-        vector<int> columns(n, -1);
+        ans.clear();
         
-        solve(ans, n, 0, 0, columns);
+        vector<int> columns(n, -1);
+        vector<bool> check(6 * n, false);
+        
+        solve(n, 0, check, columns);
         
         return ans;
     }
