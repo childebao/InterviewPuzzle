2600723c2dc3f3feb4ad16f2229631dc70fa92bd
Charlene Jiang <charlene@tsynka.com>
2013-04-26 18:45:43 +0800

    Add some optimization....

--- LeetCode/LongestValidParenthese.cpp
+++ LeetCode/LongestValidParenthese.cpp
@@ -5,32 +5,30 @@ public:
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         int ans = 0;
-        int left = 0, right = 0, start = 0;
+        int count = 0, start = 0;
         
         for (int i = 0; i < s.length(); i ++) {
-            if (s[i] == '(') left ++;
-            else right ++;
+            count = s[i] == '(' ? count + 1 : count - 1;
             
-            if (right > left) {
-                right = left = 0;
+            if (count < 0) {
+                count = 0;
                 start = i + 1;
-            } else if (right == left) {
-                ans = max(ans, left + right);  
+            } else if (count == 0) {
+                ans = max(ans, i - start + 1);  
             }
         }
 
-        if (left > right) {
-            left = right = 0;
+        if (count > 0) {
+            count = 0;
             start = s.length() - 1;
             for (int i = s.length() - 1; i >= 0; i --) {
-                if (s[i] == ')') right ++;
-                else left ++;
+                count = s[i] == ')' ? count + 1 : count - 1;
                 
-                if (left > right) {
-                    right = left = 0;
+                if (count < 0) {
+                    count = 0;
                     start = i - 1;
-                } else if (left == right) {
-                    ans = max(ans, left + right);
+                } else if (count == 0) {
+                    ans = max(ans, start - i + 1);
                 }
             }
         }
